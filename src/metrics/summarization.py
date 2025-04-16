39bcdf973bca16c7b4660dcb4ba82b096e975fa1
melalex <alexander.melashchenko@gmail.com>
2025-04-16 18:22:30 +0000

    Test lab 2

--- src/metrics/summarization.py
+++ src/metrics/summarization.py
@@ -4,10 +4,13 @@ import numpy as np
 rouge = evaluate.load("rouge")
 
 
-def compute_metrics(eval_pred, tokenizer):
-    predictions, labels = eval_pred
-    decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
-    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
-    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
-    result = rouge.compute(predictions=decoded_preds, references=decoded_labels)
-    return {k: v.mid.fmeasure for k, v in result.items()}
+def compute_metrics(tokenizer):
+    def currying(eval_pred):
+        predictions, labels = eval_pred
+        decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
+        labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+        decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+        result = rouge.compute(predictions=decoded_preds, references=decoded_labels)
+        return result
+
+    return currying
